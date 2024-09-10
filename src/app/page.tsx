534f4230f36f5e72ef6e724905d9fffd5de03d59
Supriya Gorai <115802703+supriya-gorai-p360@users.noreply.github.com>
2024-09-10 12:03:13 -0400

    2nd commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
 import FeaturesCourses from "@/components/FeaturesCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import Testimonials from "@/components/Testimonials";
+import UpcomingWebiners from "@/components/UpcomingWebiners";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <Hero/>
       <FeaturesCourses />
+      <WhyChooseUs />
+      <Testimonials/>
+      <UpcomingWebiners/>
     </main>
   );
 }
